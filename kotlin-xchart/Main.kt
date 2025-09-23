8f9dbe9c3c3007374cad0d32714bf8522678b4b7
Priya Shivaani Chauhan <p.shiv.ch@gmail.com>
2025-09-22 20:27:23 -0700

    Delete kotlin-xchart directory

--- kotlin-xchart/Main.kt
+++
@@ -1,8 +0,0 @@
-import org.knowm.xchart.*
-
-fun main() {
-    val xData = doubleArrayOf(0.0, 1.0, 2.0)
-    val yData = doubleArrayOf(2.0, 1.0, 0.0)
-    val chart = QuickChart.getChart("Scene Adaptation", "X", "Y", "y(x)", xData, yData)
-    SwingWrapper(chart).displayChart()
-}
